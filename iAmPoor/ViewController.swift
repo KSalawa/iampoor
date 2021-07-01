ce1cb18f11de7cdfa363b06dcc65c9baf6c47973
KSalawa <emir.salawa@gmail.com>
2021-07-01 19:05:26 +0100

    final

--- iAmPoor/ViewController.swift
+++ iAmPoor/ViewController.swift
@@ -6,14 +6,39 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    var player: AVAudioPlayer!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
     }
 
-
+    @IBAction func poorButton(_ sender: UIButton) {
+        print("poor pressure")
+        playSound()
+    }
+    
+    @IBAction func dont(_ sender: UIButton) {
+        print("dont")
+        poorCunt()
+    }
+    
+    
+    func playSound() {
+        let url = Bundle.main.url(forResource: "poor", withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
+    
+    func poorCunt() {
+        let url = Bundle.main.url(forResource: "dont", withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
+    
 }
 
